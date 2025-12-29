1b5dd1db35e501491b876901c9920c9e7341e9a0
zoombinipasta <zanefaraday@gmail.com>
2025-12-28 18:05:15 -0800

    bg change

--- background.gd
+++ background.gd
@@ -1,11 +1,5 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _on_game_season_change(season: int) -> void:
+	self.frame = season 
